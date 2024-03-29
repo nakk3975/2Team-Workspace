4638cb6ec4f1037ec777cad487a1453cb5418dc5
Ahn <dsfs3975@naver.com>
2024-03-29 16:29:16 +0900

    24.03.29 11:44 - 안재원 - 로그인 화면 추가 - 회원가입 화면 추가

---
+++ src/main/java/com/weather/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.weather.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/signin/view")
+	public String signin() {
+		return "/user/signin";
+	}
+	
+	@GetMapping("/signup/view")
+	public String signup() {
+		return "/user/signup";
+	}
+}
