ab114dabf5d152582500b46939df4f83c86df840
DuckchiS <rjsdn18962@daum.net>
2024-04-01 12:33:20 +0900

    4월 1일)브랜치 생성-김건우

--- src/main/java/com/weather/test/HelloController.java
+++ src/main/java/com/weather/test/HelloController.java
@@ -3,7 +3,7 @@ package com.weather.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//브랜치 생성용으로 한겁니다.
 @Controller
 public class HelloController {
 
