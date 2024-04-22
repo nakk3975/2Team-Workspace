0ca4e97559a4a899fff5f5af0c6f3dd8b8296221
YoonHyunJung <guswndeks@naver.com>
2024-04-22 10:43:59 +0900

    24.04.22 10:43 - 안재원
    getter ~ setter 생성하여 위성사진 콜 문제 해결

--- src/main/java/com/weather/satellite/dto/Response.java
+++ src/main/java/com/weather/satellite/dto/Response.java
@@ -4,11 +4,24 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class Response {
 
     public Header header;
     public Body body;
+	public Header getHeader() {
+		return header;
+	}
+	public void setHeader(Header header) {
+		this.header = header;
+	}
+	public Body getBody() {
+		return body;
+	}
+	public void setBody(Body body) {
+		this.body = body;
+	}
 
+    
 }
